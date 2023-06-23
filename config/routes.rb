5fad9fbe3e82e75f9b06b332330614720d95da1f
Sandra Campos <sandra14_lu@hotmail.coml>
2023-06-22 23:13:22 -0400

    agrego acerca de yop

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
   get '/proyectos', to: 'pages#proyectos'
+  get '/contacto', to: 'pages#contacto'
 
 end
